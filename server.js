7965e8cbc1c3734183c01f8fee9e1c45ef4185ce
Ricardo Ramos <rf.ramos95@gmail.com>
2022-02-23 11:30:10 +0000

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATBASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
   });
